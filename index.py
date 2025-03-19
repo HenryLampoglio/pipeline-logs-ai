9288372bb8b72f5b1635d26c00f0082c523d6e51
raphael <rafavfx1@gmail.com>
2025-03-19 19:33:02 -0300

    Add Extraction class and refactor Regex class usage in index.py

--- index.py
+++ index.py
@@ -1,15 +1,9 @@
 import pandas as pd
-from Regex import RegexClass
+from Regex import Regex
+from Extraction import Extraction
 
-def extract_dataset_by_state(df):
-    estado_dfs = {estado: df[df['state'] == estado] for estado in df['state'].unique()}
-
-    for estado, df_estado in estado_dfs.items():
-        path = f"datasets/datasets_tratadas/logs_{estado}.csv"
-        df_estado.to_csv(path, index=False)
-        print(f"dataset gerada para o estado de {estado}")
-
-
+regexObject = Regex()
+extractionObject = Extraction()
 df = pd.read_csv('datasets/MOCK_DATA.csv')
 # print(df.to_json())
 
@@ -22,33 +16,10 @@ news_id = df['news_id']
 time_spent = df['time_spent']
 device = df['device']
 
-news = [
-    "news/1.txt",
-    "news/2.txt",
-    "news/3.txt",
-    "news/4.txt",
-    "news/5.txt",
-    "news/6.txt",
-    "news/7.txt",
-    "news/8.txt",
-    "news/9.txt",
-    "news/10.txt",
-    "news/11.txt",
-    "news/12.txt",
-    "news/13.txt",
-    "news/14.txt",
-    "news/15.txt",
-    "news/16.txt",
-    "news/17.txt",
-    "news/18.txt",
-    "news/19.txt",
-    "news/20.txt",
-]
+news = {i: f"news/{i}.txt" for i in range(1, 21)}
 
 for index, row in df.iterrows():
-    news_id = int(row['news_id']) - 1
-
-    RegexClass().noticia_relacionada_a_esporte(news[news_id])
+    regexObject.noticia_relacionada_a_esporte(news[int(row['news_id'])])
     # print(response)
 
 # Regex.noticia_relacionada_a_esporte(news, )
