3845dfa512ac67c1118a2afcbad33b60ac561612
raphael <rafavfx1@gmail.com>
2025-03-19 20:14:05 -0300

    Add Extraction and Charts classes, and update index.py to generate pie charts and extract datasets by state

--- index.py
+++ index.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from Regex import Regex
 from Extraction import Extraction
-from Charts import Charts
+from utils.Charts import Charts
 
 
 regexObject = Regex()
 extractionObject = Extraction()
+chartsObject = Charts()
+
+news = {i: f"news/{i}.txt" for i in range(1, 21)} # declara um array indexado para armazenar o caminho das noticias na pasta news
+
 df = pd.read_csv('datasets/MOCK_DATA.csv')
+
 # print(df.to_json())
 
 timestamp = df['timestamp']
@@ -17,13 +22,13 @@ df['gender'] = df['gender'].apply(lambda x: 'M' if x == 'Male' else 'F') # caso 
 news_id = df['news_id']
 time_spent = df['time_spent']
 device = df['device']
-news = {i: f"news/{i}.txt" for i in range(1, 21)}
-df['category'] = df['news_id'].apply(lambda x: regexObject.identifyNewsCategory(news[int(x)])) # para cada iteração do news_id ele pega o 
+
+df['category'] = news_id.apply(lambda x: regexObject.identifyNewsCategory(news[int(x)])) # para cada iteração do news_id ele pega o 
                                                                                             #   valor e aplica o identify para verificar qual é a categoria 
                                                                                             #   que mais se identifica baseado em REGEX
-category_counts = df['category'].value_counts()
 
+category_counts = df['category'].value_counts() ## faz a contagem das categorias, para gerar o gráfico
 
-
+chartsObject.generatePieChart(category_counts)
 # extract_dataset_by_state(df)
 
--- utils/Charts.py
+++ utils/Charts.py
@@ -5,11 +5,9 @@ class Charts:
     def __init__(self):
         self.colors = sns.color_palette("pastel")
 
-        
+
     def generatePieChart(self, values):  
         matplot.figure(figsize=(8, 8))
-
         matplot.pie(values, labels=values.index, autopct='%1.1f%%', colors=self.colors, startangle=140)
-        matplot.title('Distribuição das Categorias de Notícias Mais Lidas')
-
+        matplot.title('Porcentagem de Categorias de Notícias Mais Lidas')
         matplot.show()
