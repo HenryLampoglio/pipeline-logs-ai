4ccc2278682c9194ab0d22f2dc5fb6242c3181b9
raphael <rafavfx1@gmail.com>
2025-04-07 22:48:41 -0300

    Add directory creation for results and datasets in Charts and Extraction modules

--- utils/Charts.py
+++ utils/Charts.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as matplot
 import seaborn as sns
+import os
 
 class Charts:
     def __init__(self):
@@ -7,6 +8,7 @@ class Charts:
 
 
     def generatePieChart(self, values, df_file):   # gera o gráfico de pizza com as porcentagens de categorias de notícias mais lidas
+        os.makedirs('results', exist_ok=True)
         matplot.figure(figsize=(8, 8))
         matplot.pie(values, labels=values.index, autopct='%1.1f%%', colors=self.colors, startangle=140)
         matplot.title(f'Porcentagem de Categorias de Notícias Mais Lidas - {df_file}')
--- utils/Extraction.py
+++ utils/Extraction.py
@@ -1,7 +1,11 @@
+import os
+
 class Extraction:
     def extract_dataset_by_state(self, df): # extrai os dados do dataframe original e cria um novo dataframe para cada estado
         df_state = {estado: df[df['state'] == estado] for estado in df['state'].unique()}
+        os.makedirs("datasets", exist_ok=True)  # Cria a pasta se não existir
+        
         for estado, df_estado in df_state.items():
-            path = f"datasets/logs_{estado}.csv"
+            path = os.path.join("datasets", f"logs_{estado}.csv")
             df_estado.to_csv(path, index=False)
-            # print(f"dataset gerada para o estado de {estado}")
+            
